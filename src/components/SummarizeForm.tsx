23a0d4b221847299ac1a29380c05158e9b3441e8
Chase Evans <chwevans@gmail.com>
2025-08-05 17:14:50 -0700

    Subtle fixes

--- src/components/SummarizeForm.tsx
+++ src/components/SummarizeForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "./Button";
 import { GenAITextFormatter } from "./GenAITextFormatter";
 import { TextContent } from "./TextContent";
@@ -13,76 +13,98 @@ export const SummarizeForm = () => {
   const [processing, setProcessing] = useState(false);
   const [error, setError] = useState<string>("");
   const [result, setResult] = useState<string | null>(null);
+  const [textArea, setTextArea] = useState<string>("");
+  const [credentials, setCredentials] = useState<string>("");
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    const credentials = new URLSearchParams(location.hash.substring(1)).get(
-      "token"
-    );
-    event.preventDefault();
+  const handleSubmit = async (text: string, credentials: string) => {
     setProcessing(true);
-    const response = fetch(apiPath("/api/chat"), {
+    const response = await fetch(apiPath("/api/chat"), {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         Redirect: "/",
-        Authorization: credentials || "",
+        Authorization: credentials,
       },
       body: JSON.stringify({
         model: "cheapest",
         messages: [
           {
             role: "user",
-            content: `Please summarize the following text:\n${event.currentTarget.textarea.value}`,
+            content: `Please summarize the following text:\n${text}`,
           },
         ],
       }),
     });
-    response
-      .then((response) => {
-        console.log(response);
-        console.log("Redirect", response.headers.get("Location"));
-        if (!response.ok) {
-          const location = response.headers.get("Location");
-          if (response.status === 401 && location) {
-            window.location.href = location;
-            return;
-          }
-          response.text().then((text) => {
-            setError("The text was unable to be summarized: " + text);
-          });
-          return;
-        }
+    setProcessing(false);
+    console.log(response);
+    if (!response.ok) {
+      const location = response.headers.get("Location");
+      if (response.status === 401 && location) {
+        console.log("Redirecting to", location);
 
-        return response.json();
-      })
-      .then((data) => {
-        setError("");
-        setResult(data.message.content);
-      })
-      .finally(() => {
-        setProcessing(false);
-      });
+        // Store the current textarea in local storage to be restored after login
+        localStorage.setItem("textarea", text);
+
+        window.location.href = location;
+      } else {
+        const responseText = await response.text();
+        setError("The text was unable to be summarized: " + responseText);
+      }
+      return;
+    }
+
+    const data = await response.json();
+    setError("");
+    setResult(data.message.content);
   };
+
+  // If we came back with a textarea value, restore it
+  useEffect(() => {
+    const credentials = new URLSearchParams(location.hash.substring(1)).get(
+      "token"
+    );
+
+    if (credentials) {
+      setCredentials(credentials);
+
+      // Remove the token from the URL
+      const oldString = window.location.toString();
+      window.history.replaceState(
+        {},
+        document.title,
+        oldString.substring(0, oldString.indexOf("#"))
+      );
+
+      const storedTextArea = localStorage.getItem("textarea");
+      if (storedTextArea) {
+        setTextArea(storedTextArea);
+        handleSubmit(storedTextArea, credentials);
+      }
+    }
+  }, []);
 
   return (
     <>
       {result === null && (
         <>
-          <form onSubmit={handleSubmit}>
-            <textarea
-              className={
-                processing
-                  ? "text-area vertical-resize gap-medium disabled"
-                  : "text-area vertical-resize gap-medium"
-              }
-              placeholder="To be or not to be..."
-              name="textarea"
-            ></textarea>
+          <textarea
+            className={
+              processing
+                ? "text-area vertical-resize gap-medium disabled"
+                : "text-area vertical-resize gap-medium"
+            }
+            placeholder="To be or not to be..."
+            name="textarea"
+            onChange={(e) => setTextArea(e.target.value)}
+            value={textArea}
+          />
 
-            <Button processing={processing} type="submit">
-              Summarize
-            </Button>
-          </form>
+          <Button
+            processing={processing}
+            onClick={() => handleSubmit(textArea, credentials)}
+          >
+            Summarize
+          </Button>
           {error !== "" && (
             <TextContent
               size="small"
